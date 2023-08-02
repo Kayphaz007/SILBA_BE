aad19ef2594d76b82ad49dae4dba1ef55e5b0e83
Nyene-Abasi <nyeneabasi3@gmail.com>
2023-08-02 13:46:36 +0100

    changes

--- controllers/business.js
+++ controllers/business.js
@@ -4,3 +4,7 @@ exports.getAllBusiness = async (req, res, next) =>{
     const business = await Business.find({})
     res.status(200).send({ business })
 }
+
+exports.postBusiness = async(req, res, next) =>{
+    
+}
