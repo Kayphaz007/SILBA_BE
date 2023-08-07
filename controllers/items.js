ed64614e8a8ef2d144d29790e7196e1e34b72770
Nyene-Abasi <nyeneabasi3@gmail.com>
2023-08-07 10:16:40 +0100

    cloudinary upload of images

--- controllers/items.js
+++ controllers/items.js
@@ -1,4 +1,12 @@
 const Item = require("../models/itemSchema")
+const cloudinary = require('cloudinary').v2
+
+          
+cloudinary.config({ 
+  cloud_name: 'drvaa1jgo', 
+  api_key: '745795443419229', 
+  api_secret: 'w3zpgq-Ck24Ja7DtW4vAFLZ4WSo' 
+});
 
 exports.getAllItems = async (req, res, next) => {
     const item = await Item.find({});
@@ -13,13 +21,28 @@ exports.getItemById = async (req, res, next) => {
 
 exports.postItem = async (req, res, next) => {
     try {
-const {itemname, description, image, price, refUser} = req.body
-const item = await Item.create({itemname, description, image, price, refUser})
-res.status(200).send({data: item})
-    }catch (err){
-        console.log(err, "error posting items")
+      const { itemname, description, price, refUser } = req.body;
+      const image = req.file; 
+      const result = await cloudinary.uploader.upload(image.path, {
+        resource_type: 'image',
+        folder: 'silba-project',
+      });
+      console.log(result)
+  
+      const item = await Item.create({
+        itemname,
+        description,
+        image: result.secure_url,
+        price,
+        refUser,
+      });
+  
+      res.status(200).send({ data: item });
+    } catch (err) {
+      console.log(err, 'error posting items');
+      res.status(500).send('Error posting items');
     }
-}
+  };
 
 exports.deleteItem = async (req, res, next) => {
     const {itemId} = req.params
--- routes/items.js
+++ routes/items.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const router = express.Router();
 const {getAllItems, getItemById, postItem, deleteItem} = require("../controllers/items")
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  });
+  const upload = multer({ storage });
 
 router.route("/").get(getAllItems);
 router.route("/:itemId").get(getItemById);
-router.route("/").post(postItem);
+router.route('/').post(upload.single('image'), postItem);
 router.route("/:itemId").delete(deleteItem)
 
 module.exports = router;
